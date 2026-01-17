4f251aff67cca00cba3b802f609fb5d6fc5c1164
Sudheendra Sripada <choppasreepada@gmail.com>
2026-01-17 12:56:27 +0530

    Delete ai for coders directory

--- ai for coders/index.tsx
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './src/app.component';
-
-
-bootstrapApplication(AppComponent)
-  .catch(err => console.error(err));
-
-// AI Studio always uses an `index.tsx` file for all project types.
